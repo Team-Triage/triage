525abe6d5eac52e2223d063b2c9a9f5592a22a1f
mhtjung <themikejung@gmail.com>
2022-11-03 13:05:02 -0700

    Update dummyDispatch
    Added timeout to simulate grpc call (and avoid concurrent map write error)
    
    Co-Authored-By: aashish-b <87250229+aashish-b@users.noreply.github.com>
    Co-Authored-By: Aryan Binazir <65728619+aryan-binazir@users.noreply.github.com>
    Co-Authored-By: Jordan Swartz <10202436+jordanLswartz@users.noreply.github.com>

--- dev/tmp/dummyDispatch.go
+++ dev/tmp/dummyDispatch.go
@@ -13,7 +13,7 @@ func DummyDispatch() {
 		msg := toDispatch.GetMessage()
 		fmt.Println("DummyDispatch received message")
 		// make grpc call
-		time.Sleep(time.Millisecond * 5)
+		time.Sleep(time.Millisecond * 5) // delay to simulate grpc call
 		var ack *types.Acknowledgment = &types.Acknowledgment{Offset: int(msg.TopicPartition.Offset), Status: 1, Event: msg}
 		fmt.Printf("DummyDispatch sending message at offset %v to filter\n", ack.Offset)
 		toFilter.AppendMessage(ack)
